6ce49839d6ff8c6e3d7a0856692d625b3fa07716
erroshan06 <rroshankum908@gmail.com>
2025-08-19 20:53:33 +0530

    coomit

---
+++ src/ARRAY/InputOutputArray.java
@@ -0,0 +1,16 @@
+package ARRAY;
+
+import java.util.Scanner;
+
+public class InputOutputArray {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner( System.in);
+        int  n = sc.nextInt();
+        int []arr  = {1,2,3,4,5,6,7,8,-8,-4};
+
+        for(int i= 0 ; i<= 10; i++){
+            System.out.println();
+        }
+    }
+}
