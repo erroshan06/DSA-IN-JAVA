2fc9399893bbb2744f25524ff0bbd2d518f9efc3
erroshan06 <rroshankum908@gmail.com>
2026-02-08 23:11:54 +0530

    commit

--- src/ARRAY/Sorting/Bubblesort.java
+++ src/ARRAY/Sorting/Bubblesort.java
@@ -1,31 +1,46 @@
 package ARRAY.Sorting;
 
 public class Bubblesort {
+
     public static void print(int[] arr) {
-        for (int ele : arr)
-            System.out.print( ele  + " ");
+
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
         System.out.println();
     }
 
+
     public static void main(String[] args) {
 
+        int[] arr = {3, 45, 6, 2, 8, -9, 0};
 
-        int[] arr = {2, 3, 45, 21, 46, 3, 2, 5};
-        int n = arr.length;
-        print(arr);
-         for (int i= 0; i<n-1 ; i++){
-            for (int j = 0; j < n - 1; j++) {
-                if (arr[j] > arr[j+ 1]) {
+
+
+
+
+        for (int i = 0; i < arr.length - 1; i++) {     // n-1 passes
+            boolean isSorted = true ;
+            for (int j = 0; j < arr.length - 1; j++) {   // to check  array is sorted or not
+
+                if (arr[j] > arr[j + 1]) {
+                    isSorted = false;
+                    break;
+                }
+            }
+             if (isSorted == true) break ;
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
-
-
                 }
+
+
             }
-
         }
-         print(arr);
+        print(arr);
     }
-
 }
